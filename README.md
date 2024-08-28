ebf944d31bc53c3fc68dc1d247d4d7249ea52271
Ricardo Michelangelo de Sá <ricardo.mdesa@gmail.com>
2024-08-28 08:33:42 -0300

    adjustments and readme

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 
 [link video streaming-server](https://www.rohitmundra.com/video-streaming-server)
 
+# Startup server
+Run
+```bash
+go build cmd/api/main.go && ./main
+```
+
+The videos must be in assets/media folder and converted to m3u8 HLS format
+
 ## Speed up video in 1.5x.
 
 ```bash
-ffmpeg -i 5-Percorrendo\ Arrays.mp4 -filter_complex "[0:v]setpts=0.6667*PTS[v];[0:a]atempo=1.5[a]" -map "[v]" -map "[a]" video5_1_5x.mp4
+ffmpeg -i 5-video.mp4 -filter_complex "[0:v]setpts=0.6667*PTS[v];[0:a]atempo=1.5[a]" -map "[v]" -map "[a]" video5_1_5x.mp4
 ```
 
 ## Convert video to hls format
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,73 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
+	"github.com/ricardomdesa/videostr/api/route"
+	log "github.com/sirupsen/logrus"
 )
 
-
 func main() {
-	fmt.Println("Starting video server...")
-	http.Handle("/", handlers())
-	http.ListenAndServe(":8000", nil)
-}
+	log.Info("Starting video server...")
+	r := gin.Default()
 
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
-}
+	route.Setup(r)
+	gin.SetMode(gin.DebugMode)
 
-func handlers() *mux.Router {
-	router := mux.NewRouter()
-	router.HandleFunc("/", indexPage).Methods("GET")
-	// router.HandleFunc("/media/{mId:[0-9]+}/stream/", streamHandler).Methods("GET")
-	// router.HandleFunc("/media/{mId:[0-9]+}/stream/{segName:index[0-9]+.ts}", streamHandler).Methods("GET")
-	return router
-}
-
-func indexPage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
-}
-
-func streamHandler(response http.ResponseWriter, request *http.Request) {
-	enableCors(&response)
-	vars := mux.Vars(request)
-	mId, err := strconv.Atoi(vars["mId"])
-	if err != nil {
-		response.WriteHeader(http.StatusNotFound)
-		return
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
 	}
-
-	segName, ok := vars["segName"]
-	if !ok {
-		mediaBase := getMediaBase(mId)
-		m3u8Name := "index.m3u8"
-		serveHlsM3u8(response, request, mediaBase, m3u8Name)
-	} else {
-		mediaBase := getMediaBase(mId)
-		serveHlsTs(response, request, mediaBase, segName)
-	}
-}
-
-func getMediaBase(mId int) string {
-	mediaRoot := "assets/media"
-	return fmt.Sprintf("%s/%d", mediaRoot, mId)
-}
-
-func serveHlsM3u8(w http.ResponseWriter, r *http.Request, mediaBase, m3u8Name string) {
-	mediaFile := fmt.Sprintf("%s/%s", mediaBase, m3u8Name)
-	fmt.Printf("media file: %s\n", mediaFile)
-	w.Header().Set("Content-Type", "application/x-mpegURL")
-	http.ServeFile(w, r, mediaFile)
-}
-
-func serveHlsTs(w http.ResponseWriter, r *http.Request, mediaBase, segName string) {
-	mediaFile := fmt.Sprintf("%s/%s", mediaBase, segName)
-	http.ServeFile(w, r, mediaFile)
-	w.Header().Set("Content-Type", "video/MP2T")
 }
